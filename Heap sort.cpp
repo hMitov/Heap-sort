db00832a80edbe9987a1b9ff125600adca41e8eb
Hristiyan Mitov <67628947+hMitov@users.noreply.github.com>
2021-10-01 14:51:21 +0300

    Create Heap sort.cpp

---
+++ Heap sort.cpp
@@ -0,0 +1,89 @@
+#include <iostream> 
+
+using namespace std;
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2; 
+
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+
+        heapify(arr, n, largest);
+    }
+}
+
+
+void insertNode(int arr[], int& n, int key) {
+    n = n + 1;
+
+    arr[n - 1] = key;
+}
+
+
+void deleteEdge(int arr[], int& n, int edge)
+{
+    int lastElement = arr[n - 1];
+
+    arr[edge] = lastElement;
+
+    n = n - 1;
+}
+ 
+
+void heapSort(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+
+    for (int i = n - 1; i > 0; i--) {
+        swap(arr[0], arr[i]);
+
+        heapify(arr, i, 0);
+    }
+}
+
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    int* arr = new int[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    heapSort(arr, n);
+    printArray(arr, n);
+
+    insertNode(arr, n, 10);
+    printArray(arr, n);
+
+    heapSort(arr, n);
+    cout << "Sorted array is \n";
+    printArray(arr, n);
+
+    deleteEdge(arr, n, 3);
+    heapSort(arr, n);
+    cout << "Sorted array is \n";
+    printArray(arr, n);
+
+    return 0;
+}
